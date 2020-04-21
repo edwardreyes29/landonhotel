08e7a0b957cd4ffd768b46d3885c39c3521dc7c3
Edward Reyes <reyes.edwardf@gmail.com>
2020-04-20 19:33:30 -0700

    update time in events

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 (function() {
   "use strict";
 
+  document.addEventListener("DOMContentLoaded", function() {
+    var c = document.getElementById("current-time");
+
+    var d = new Date(); // Gets date from right now
+
+    var hours = d.getHours();
+    if (hours > 12) {
+      hours -= 12;
+    }
+    c.innerHTML = hours + ":" + d.getMinutes();
+  });
+
 })();
