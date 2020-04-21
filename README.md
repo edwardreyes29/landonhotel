25c50f7c715d5e0232e63cb0e9157d6285117e37
Edward Reyes <reyes.edwardf@gmail.com>
2020-04-20 20:20:03 -0700

    add current date

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # landonhotel
+
+In this project, I learned how to implement JavaScirpt setIntervals to get live and continous updates
+on the current Date and Time and display both in the elements inner HTML.
+
+# Features
+Get current Date and Time by updating them using JavaScript setInterval()
+![Submit Form Disabled](assets/screenshot-1.png)
--- script.js
+++ script.js
@@ -3,14 +3,56 @@
 
   document.addEventListener("DOMContentLoaded", function() {
     var c = document.getElementById("current-time");
+    var dt = document.getElementById("current-date");
 
-    var d = new Date(); // Gets date from right now
+    setInterval(updateTime, 1000); // setInterval updates continuously
 
-    var hours = d.getHours();
-    if (hours > 12) {
-      hours -= 12;
+    function updateTime() {
+      var d = new Date(); // Gets date from right now
+      var hours = d.getHours(),
+        ampm = 'AM',
+        minutes = d.getMinutes(),
+        monthInt = d.getMonth(),
+        month = "",
+        day = d.getDate();
+
+        monthInt = parseInt(monthInt, 10);
+
+        var monthNames = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+        ]
+
+      if (hours > 12) {
+        hours -= 12;
+        ampm = 'PM';
+      } else if (hours === 0) { // deals with midnight being 0 instead of 12
+        hours = 12; 
+      }
+
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+
+      var sepClass = '';
+
+      if (d.getSeconds() % 2 === 0) sepClass = 'trans';
+      
+      var sep = "<span class='" + sepClass + "'>:</span>";
+      dt.innerHTML = monthNames[monthInt] + " " + day;
+      c.innerHTML = hours + sep + minutes + " " + ampm;
     }
-    c.innerHTML = hours + ":" + d.getMinutes();
+
   });
 
 })();
